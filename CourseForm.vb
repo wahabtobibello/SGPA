9eeb669d6e29ad38a69f24df0957c39d3c37e13c
Wahab Bello <wahaaabello@gmail.com>
2021-03-11 08:02:04 +0100

    Fix issue with validation

--- CourseForm.vb
+++ CourseForm.vb
@@ -1,19 +1,16 @@
 ﻿Public Class CourseForm
-    Dim courses(7) As Course
-    Dim student As Student
-    Public Sub New(student As Student)
-        Me.student = student
-    End Sub
+    Dim courses As New List(Of Course)
+    Public student As Student
     Private Sub addBtn_Click(sender As Object, e As EventArgs) Handles addBtn.Click
         If courseCodeTbx.Text = "" Or unitTbx.Text = "" Then
             MessageBox.Show("Please fill all fields")
-        ElseIf courses.Length >= 8 Then
+        ElseIf courses.Count >= 8 Then
             MessageBox.Show("You have added enough courses")
         Else
             Dim courseCode As String = courseCodeTbx.Text
             Dim unit As String = unitTbx.Text
             Dim course = New Course(courseCode, unit)
-            courses.Append(course)
+            courses.Add(course)
             Dim item As ListViewItem = New ListViewItem()
             item.Text = course.courseCode + " (" + course.unit + " unit(s))"
             courseListView.Items.Add(item)
@@ -29,7 +26,7 @@
     End Sub
 
     Private Sub submitBtn_Click(sender As Object, e As EventArgs) Handles submitBtn.Click
-        If courses.Length = 0 Then
+        If courses.Count = 0 Then
             MessageBox.Show("Please add atleast one course")
         Else
 
--- StudentBioDataForm.vb
+++ StudentBioDataForm.vb
@@ -1,15 +1,20 @@
 ﻿Public Class StudentBioDataForm
     Dim form2 As CourseForm
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles submitBtn.Click
-        Dim student As Student = New Student()
-        student.fullName = fullNameTbx.Text
-        student.faculty = facultyTbx.Text
-        student.department = departmentTbx.Text
-        student.matricNo = Convert.ToInt32(matricNoTbx.Text)
-        student.year = Convert.ToInt32(yearTbx.Text)
+        If fullNameTbx.Text = "" Or facultyTbx.Text = "" Or departmentTbx.Text = "" Or matricNoTbx.Text = "" Or yearTbx.Text = "" Then
+            MessageBox.Show("Please fill all fields")
+        Else
+            Dim student As Student = New Student()
+            student.fullName = fullNameTbx.Text
+            student.faculty = facultyTbx.Text
+            student.department = departmentTbx.Text
+            student.matricNo = Convert.ToInt32(matricNoTbx.Text)
+            student.year = Convert.ToInt32(yearTbx.Text)
 
-        form2 = New CourseForm(student)
-        form2.ShowDialog()
+            form2 = New CourseForm()
+            form2.student = student
+            form2.ShowDialog()
+        End If
     End Sub
 
     Private Sub unitTbx_KeyPress(sender As Object, e As KeyPressEventArgs) Handles matricNoTbx.KeyPress, yearTbx.KeyPress
